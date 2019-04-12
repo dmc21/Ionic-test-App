489e641c2c1c54d93f26f9c8f7d0dd9b4dd43a51
David Mora <davidmoracaceres20@gmail.com>
2019-04-12 09:18:53 +0200

    ionic hello world

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  constructor(private alertController: AlertController, private menu: MenuController) { }
+
+  async sayHello() {
+    const alert = await this.alertController.create({
+      header: 'Saludo',
+      subHeader: 'Bienvenido a mi primera aplicación!',
+      message: 'Soy un alert!',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  openEnd() {
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+  }
 
 }
